531feaf7e773fb77b5003cb106b1fc33494288a4
lucky-patel-7 <125451527+lucky-patel-7@users.noreply.github.com>
2023-03-15 14:10:23 +0530

    updated

--- backend/__test__/Attendance.test.js
+++ backend/__test__/Attendance.test.js
@@ -4,8 +4,8 @@ const app = require("../app");
 
 
 beforeAll(async () => {
-  await mongoose.connect("mongodb+srv://luckypatel:sttl123@cluster0.z6pvres.mongodb.net/?retryWrites=true&w=majority"
-  );
+  // await mongoose.connect("mongodb://localhost:27017/");
+  await mongoose.connect("mongodb+srv://luckypatel:sttl123@cluster0.z6pvres.mongodb.net/?retryWrites=true&w=majority");
 });
 
 // // /* Dropping the database and closing connection after each test. */
@@ -18,7 +18,7 @@ describe("Student Test case", () => {
 
   describe("Check IN Out", () => {
     it("Check IN Out ", async () => {
-      const response = await request(app).get("/student/checkInOut/63edf5c63fc34460ceefe1c5");
+      const response = await request(app).get("/api/student/checkInOut/63edf5c63fc34460ceefe1c5");
       expect(response.statusCode).toBe(200);
     });
   });
@@ -27,7 +27,7 @@ describe("Student Test case", () => {
   describe("register student", () => {
     it("register student ", async () => {
       //test case for register student
-      const response = await request(app).post("/student/register").send(
+      const response = await request(app).post("/api/student/register").send(
         {
           "Name": "test Bhai",
           "Email": "test@gmail.com",
@@ -40,7 +40,7 @@ describe("Student Test case", () => {
     });
 
     it("register student with same email", async () => {
-      const response = await request(app).post("/student/register").send(
+      const response = await request(app).post("/api/student/register").send(
         {
           "Name": "test Bhai",
           "Email": "test@gmail.com",
@@ -57,7 +57,7 @@ describe("Student Test case", () => {
     //test case for register student without email
 
     it("register student without email", async () => {
-      const response = await request(app).post("/student/register").send(
+      const response = await request(app).post("/api/student/register").send(
         {
           "Name": "test Bhai",
           "Gender": "Male",
@@ -77,7 +77,7 @@ describe("Student Test case", () => {
   describe('POST /sign-in', () => {
     it('should return a JWT token when the correct email and password are provided', async () => {
       const response = await request(app)
-        .post('/student/sign_in')
+        .post('/api/student/sign_in')
         .send({ Email: 'test@gmail.com', Password: 'Admin@123' });
       expect(response.statusCode).toBe(200);
       expect(response.body["data"][0]).toHaveProperty('Token');
@@ -85,7 +85,7 @@ describe("Student Test case", () => {
 
     it('should return a 500 error when an incorrect Password is provided', async () => {
       const response = await request(app)
-        .post('/student/sign_in')
+        .post('/api/student/sign_in')
         .send({ Email: 'test@gmail.com', Password: 'Adminn@1234' });
       expect(response.body["code"]).toBe("500");
       expect(response.body["message"]).toEqual('Authentication failed. Invalid user or password.');
@@ -93,7 +93,7 @@ describe("Student Test case", () => {
 
     it('should return a 500 error when an incorrect Email is provided', async () => {
       const response = await request(app)
-        .post('/student/sign_in')
+        .post('/api/student/sign_in')
         .send({ Email: 'test123@gmail.com', Password: 'Admin@123' });
 
       expect(response.body["code"]).toBe("500");
@@ -103,7 +103,7 @@ describe("Student Test case", () => {
 
   describe("leaveApplication", () => {
     it("leaveApplication ", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "studentId": "63edf9c867f386077a512a2a",
           "leaveType": "Leave",
@@ -117,7 +117,7 @@ describe("Student Test case", () => {
     });
 
     it("leaveApplication without studentId", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "leaveType": "Leave",
           "leaveFrom": "2023-02-17",
@@ -130,7 +130,7 @@ describe("Student Test case", () => {
     });
 
     it("leaveApplication without leaveType", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "studentId": "63edf9c867f386077a512a2a",
           "leaveFrom": "2023-02-17",
@@ -143,7 +143,7 @@ describe("Student Test case", () => {
     });
 
     it("leaveApplication without leaveFrom", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "studentId": "63edf9c867f386077a512a2a",
           "leaveType": "Leave",
@@ -156,7 +156,7 @@ describe("Student Test case", () => {
     });
 
     it("leaveApplication without leaveTo", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "studentId": "63edf9c867f386077a512a2a",
           "leaveType": "Leave",
@@ -169,7 +169,7 @@ describe("Student Test case", () => {
     });
 
     it("leaveApplication without reason", async () => {
-      const response = await request(app).post("/student/leaveApplication").send(
+      const response = await request(app).post("/api/student/leaveApplication").send(
         {
           "studentId": "63edf9c867f386077a512a2a",
           "leaveType": "Leave",
@@ -185,7 +185,7 @@ describe("Student Test case", () => {
 
   describe("monthSummary", () => {
     it("monthSummary ", async () => {
-      const response = await request(app).get("/student/monthSummary/63edf5c63fc34460ceefe1c5")
+      const response = await request(app).get("/api/student/monthSummary/63edf5c63fc34460ceefe1c5")
       expect(response.body["code"]).toBe("200");
       expect(response.body["flag"]).toBe(true);
     });
